8e7736f7f0f518fe3d621a3cd798fee2b24164fc
agarwal.s.vivek <agarwal.s.vivek@gmail.com>
2024-10-19 10:06:57 -0400

    migrated to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,67 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import { resolve as _resolve, join } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
-module.exports = {
-  entry: './src/index.js', // Update this if your entry file is different
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    static: {
-        directory: path.join(__dirname, 'build'),
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const WebPack = {
+    mode: 'development', // or 'production'
+    entry: './src/index.tsx', // Update this if your entry file is different
+    output: {
+        filename: 'bundle.js',
+        path: _resolve(__dirname, 'build'),
+        clean: true,
     },
-    compress: true,
-    port: 3000,
-    historyApiFallback: true, // For client-side routing
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html', // Update this if your template file is different
-    }),
-  ],
-  mode: 'development', // Change to 'production' for production builds
-};
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif])$/i,
+                type: 'asset/resource',
+            },
+            // Add more rules for other file types (images, fonts, etc.) as needed
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'build'),
+        },
+        compress: true,
+        port: 3000,
+    },
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico'
+        }),
+        new CleanWebpackPlugin(),
+    ]
+}
+export default WebPack;
